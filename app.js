11b5e0a017d23046dbd2bce8e6e18f5a21d4a164
Alvin Kwon <khh2302@hotmail.com>
2018-01-02 00:00:45 +0900

    5.제품 리스트 페이지 작성

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
-
+var path = require('path');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
 //MongoDB 접속
 var mongoose = require('mongoose');
 var autoIncrement = require('mongoose-auto-increment');
@@ -18,10 +20,19 @@ var admin = require('./routes/admin');
 var app = express();
 var port = 3000;
 
+// 확장자가 ejs 로 끈나는 뷰 엔진을 추가한다.
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// 미들웨어 셋팅
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req,res){
     res.send('first app');
 });
+
 // Routing
 app.use('/admin', admin);
 
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var ProductsModel = require('../models/ProductsModel');
 
 router.get('/', function(req,res){
     res.send('admin app');
+});
+
+router.get('/products', function(req,res){
+    res.render('admin/products', 
+        { message : "hello" });
+});
+
+router.get('/products/write', function(req,res){
+    res.render( 'admin/form');
+});
+
+router.post('/products/write', function(req,res){
+
+    var product = new ProductsModel({
+        name : req.body.name,
+        price : req.body.price,
+        description : req.body.description,
+    });
+    product.save(function(err){
+        res.redirect('/admin/products');
+    });
+
 });
 
 module.exports = router;
